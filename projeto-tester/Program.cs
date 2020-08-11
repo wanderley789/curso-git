e1c7544ef1c78374b87b647fd66cc10299ac5caa
Wanderley <pingo20112011@live.com>
2020-08-11 18:10:12 -0300

    Projeto iniciado mostrando a mensagem 'Olá mundo'

---
+++ projeto-tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace projeto_tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
