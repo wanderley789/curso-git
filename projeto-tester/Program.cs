e70d7adba31c281c6046aba7fe17037fbd21ed5d
Wanderley <pingo20112011@live.com>
2020-08-11 19:36:15 -0300

    Foi incrementado a frase 'Tenha um bom dia'

--- projeto-tester/Program.cs
+++ projeto-tester/Program.cs
@@ -7,8 +7,8 @@ namespace projeto_tester
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia !! ");
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!!");
             Console.ReadLine();
 
             
