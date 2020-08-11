5187a1b7b589c00c3613629d8c960a95fdecbde2
Wanderley <pingo20112011@live.com>
2020-08-11 18:14:58 -0300

    Projeto teste alterado, foi incrementado a frase 'Bom dia git status '

--- projeto-tester/Program.cs
+++ projeto-tester/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_tester
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia !! ");
             Console.ReadLine();
 
             
